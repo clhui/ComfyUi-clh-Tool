dee35feff7fe90f984d58e30b32977f88411adc5
chenlinhui7 <chenlinhui7@jd.com>
2024-10-29 23:09:41 +0800

    fix(web_version): 修复字符串拼接问题,移除多余的中文冒号

--- web_version/clhTool.js
+++ web_version/clhTool.js
@@ -85,7 +85,7 @@ app.registerExtension({
 					let values = message["value"].toString().split('x');
 					const result_to_label = this.widgets.find(w => w.name === "result_to_label")["value"];
 					if(result_to_label){
-						this.title = result_to_label+ "：" + values;
+						this.title = result_to_label+ values;
 					}
 					if (typeof string!== "undefined" && string!== null && string!== "") {
                         console.log("字符串不是空的，也不是 null");
